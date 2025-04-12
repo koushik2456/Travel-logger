7bef8fc7f9ae89070a8ace56273420256cf39552
koushik2456 <152166214+koushik2456@users.noreply.github.com>
2025-04-12 14:46:37 +0000

    Initial setup complete

---
+++ frontend/app.js
@@ -0,0 +1,7 @@
+async function callBackend() {
+    const res = await fetch("http://127.0.0.1:8000/");
+    const data = await res.json();
+    document.getElementById("output").textContent = data.message;
+  }
+
+  
